70ff22923a2fa1abb57c0820ecaf5bdfc48a51ea
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-10-01 01:33:49 +0600

    <Html> should not be imported outside of pages/_document.

--- app/not-found.js
+++ app/not-found.js
@@ -1,4 +1,5 @@
-export default function Custom404() {
+// app/not-found.js
+export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Page Not Found</h1>
